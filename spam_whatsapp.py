013a08405ca9aadd405129e627aae11cb87d5bdc
محمد زارعی <74659732+Mohammadzaree88@users.noreply.github.com>
2021-03-02 15:49:39 +0330

    Add files via upload

---
+++ spam_whatsapp.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from time import sleep
+
+
+class color : 
+    GREEN = '\033[92m'
+    RED = '\033[91m'
+    WHITE = '\033[0m'
+
+
+driver = webdriver.Chrome()
+driver.get("https://web.whatsapp.com")
+
+print(color.GREEN + "[0]" + color.WHITE + " create by " + color.RED + "MOHAMMAD.ZAREI \n")
+
+input("ENTER >>> ")
+
+# driver.find_element_by_xpath("//span[contains(text, '')]").click()
+# sleep(5)
+
+
+name = input("Name target >>> ")
+text = input("matn >>> ")
+time = int(input("adad >>> "))
+
+# input("ENTER TO START >>> ")
+
+for i in range(time):
+    ex = driver.find_element_by_xpath("//span[@title ='{}']".format(name))
+    ex.click
+    sleep(1)
+    driver.find_elements_by_class_name('_2_1wd ')[1].send_keys(text, i)
+    sleep(0)
+    driver.find_elements_by_class_name('_1E0Oz')[0].click()
